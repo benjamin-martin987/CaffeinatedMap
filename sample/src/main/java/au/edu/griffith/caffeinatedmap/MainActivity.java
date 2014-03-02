37ac7a66d5f9773c99e69da5445dcd8055f100ce
Benjamin Martin <benjamin.martin987@gmail.com>
2014-03-02 14:40:57 +1000

    First Customisable Sample:  - Created the MainActivity to be the first screen the user hits where they are able to adjust the ClusteringSettings.  - The MapActivity then loads the CaffeinatedMap with the defined ClusteringSettings.

--- sample/src/main/java/au/edu/griffith/caffeinatedmap/MainActivity.java
+++ sample/src/main/java/au/edu/griffith/caffeinatedmap/MainActivity.java
@@ -1,94 +1,99 @@
 package au.edu.griffith.caffeinatedmap;
 
 import android.app.Activity;
-import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
-import com.google.android.gms.maps.model.BitmapDescriptor;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+public class MainActivity extends Activity implements View.OnClickListener {
 
-import java.util.HashMap;
+    static final String NUMBER_OF_CMO_TAG = "numberofcmo";
+    static final String CLUSTER_SIZE_TAG = "clustersize";
+    static final String CAFFEINATED_CLUSTER_ICON_TAG = "cci";
+    static final String TYPE_COUNTING_TAG = "typecounting";
+    static final String CMO_VISIBILITY_CHANGE_TAG = "cmovischangeS";
 
-import au.edu.griffith.caffeinatedmap.clustering.Cluster;
-import au.edu.griffith.caffeinatedmap.clustering.ClusteringSettings;
-import au.edu.griffith.caffeinatedmap.markers.CaffeinatedClusterIcon;
-
-public class MainActivity extends Activity {
-
-    private static final String MAP_FRAGMENT_TAG = "caffeinatedmap";
-
-    private CaffeinatedMapFragment mMapFragment;
-    private CaffeinatedMap mCaffeinatedMap;
+    private TextView mNoCMO, mClusterSize;
+    private SeekBar mSBNoCMO, mSBClusterSize;
+    private CheckBox mCCI, mTypeCounting, mCMOVisChange;
+    private Button mStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setUpMapFragment();
+        init();
+    }
+
+    private void init() {
+        mNoCMO = (TextView) findViewById(R.id.tvNoCMO);
+        mClusterSize = (TextView) findViewById(R.id.tvClusterSize);
+        mSBNoCMO = (SeekBar) findViewById(R.id.sbNoCMO);
+        mSBNoCMO.setProgress(Integer.parseInt(mNoCMO.getText().toString()));
+        mSBClusterSize = (SeekBar) findViewById(R.id.sbClusterSize);
+        mSBClusterSize.setProgress(Integer.parseInt(mClusterSize.getText().toString()));
+        mCCI = (CheckBox) findViewById(R.id.cbCCI);
+        mTypeCounting = (CheckBox) findViewById(R.id.cbTypeCounting);
+        mCMOVisChange = (CheckBox) findViewById(R.id.cbCMOVisChange);
+        mStart = (Button) findViewById(R.id.bStart);
+
+        mSBNoCMO.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                mNoCMO.setText(Integer.toString(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        mSBClusterSize.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                mClusterSize.setText(Integer.toString(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        mCCI.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mTypeCounting.setChecked(isChecked);
+            }
+        });
+
+        mStart.setOnClickListener(this);
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        setUpMapIfNeeded();
-
-        RandomMarkers randomMarkers = new RandomMarkers(1000);
-        mCaffeinatedMap.addClusterableMarkers(randomMarkers.getCMOList());
-    }
-
-    private void setUpMapFragment() {
-        mMapFragment = (CaffeinatedMapFragment) getFragmentManager().findFragmentByTag(MAP_FRAGMENT_TAG);
-        if (mMapFragment == null) {
-            mMapFragment = CaffeinatedMapFragment.newInstance();
-            FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-            fragmentTransaction.add(R.id.container, mMapFragment, MAP_FRAGMENT_TAG).commit();
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bStart:
+                Intent intent = new Intent(this, MapActivity.class);
+                intent.putExtra(NUMBER_OF_CMO_TAG, mSBNoCMO.getProgress());
+                intent.putExtra(CLUSTER_SIZE_TAG, mSBClusterSize.getProgress());
+                intent.putExtra(CAFFEINATED_CLUSTER_ICON_TAG, mCCI.isChecked());
+                intent.putExtra(TYPE_COUNTING_TAG, mTypeCounting.isChecked());
+                intent.putExtra(CMO_VISIBILITY_CHANGE_TAG, mCMOVisChange.isChecked());
+                startActivity(intent);
+                break;
         }
     }
-
-    private void setUpMapIfNeeded() {
-        if (mCaffeinatedMap == null) {
-            mCaffeinatedMap = mMapFragment.getCaffeinatedMap();
-            if (mCaffeinatedMap != null) {
-                mCaffeinatedMap.setMyLocationEnabled(true);
-                mCaffeinatedMap.getClusteringSettings().setTypeCountingEnabled(true);
-
-                final HashMap<String, Integer> colours = new HashMap<String, Integer>();
-                colours.put("Fred", getResources().getColor(R.color.red));
-                colours.put("Jim", getResources().getColor(R.color.green));
-                colours.put("Sally", getResources().getColor(R.color.blue));
-                colours.put("Haley", getResources().getColor(R.color.pink));
-                colours.put("Unknown", getResources().getColor(R.color.black));
-                mCaffeinatedMap.getClusteringSettings().setOnClusterIconSelect(new ClusteringSettings.OnClusterIconSelect() {
-                    @Override
-                    public BitmapDescriptor selectIconFor(Cluster cluster) {
-                        String[] type = cluster.getTypeList();
-                        int[] d = new int[type.length];
-                        int[] c = new int[d.length];
-                        int i = 0;
-                        for (String s : type) {
-                            d[i] = cluster.getTypeCount(s);
-                            c[i] = colours.get(s);
-                            i++;
-                        }
-                        CaffeinatedClusterIcon caffeinatedClusterIcon = new CaffeinatedClusterIcon(getApplicationContext(), null, d, c);
-                        return BitmapDescriptorFactory.fromBitmap(caffeinatedClusterIcon.toBitmap());
-                    }
-                });
-
-                mCaffeinatedMap.getClusteringSettings().setOnCMOVisibilityChange(new ClusteringSettings.OnCMOVisibilityChange() {
-                    @Override
-                    public void onCMOVisible(String key) {
-                        Log.e("au.edu.griffith.caffeinatedmap", "Visible: " + key);
-                    }
-
-                    @Override
-                    public void onCMOHidden(String key) {
-                        Log.e("au.edu.griffith.caffeinatedmap", "Hidden: " + key);
-                    }
-                });
-            }
-        }
-    }
-
 }
---
+++ sample/src/main/java/au/edu/griffith/caffeinatedmap/MapActivity.java
@@ -0,0 +1,119 @@
+package au.edu.griffith.caffeinatedmap;
+
+import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+
+import java.util.HashMap;
+
+import au.edu.griffith.caffeinatedmap.clustering.Cluster;
+import au.edu.griffith.caffeinatedmap.clustering.ClusteringSettings;
+import au.edu.griffith.caffeinatedmap.markers.CaffeinatedClusterIcon;
+
+public class MapActivity extends Activity {
+
+    private static final String MAP_FRAGMENT_TAG = "caffeinatedmap";
+
+    private CaffeinatedMapFragment mMapFragment;
+    private CaffeinatedMap mCaffeinatedMap;
+
+    private int mNumberOfCMO;
+    private int mClusterSize;
+    private boolean mCaffeinatedClusterIconEnabled;
+    private boolean mTypeCountingEnabled;
+    private boolean mCMOVisibilityChangeEnabled;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        getArguments();
+        setUpMapFragment();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+        loadSettings();
+    }
+
+    private void getArguments() {
+        Bundle args = getIntent().getExtras();
+        mNumberOfCMO = args.getInt(MainActivity.NUMBER_OF_CMO_TAG);
+        mClusterSize = args.getInt(MainActivity.CLUSTER_SIZE_TAG);
+        mCaffeinatedClusterIconEnabled = args.getBoolean(MainActivity.CAFFEINATED_CLUSTER_ICON_TAG);
+        mTypeCountingEnabled = args.getBoolean(MainActivity.TYPE_COUNTING_TAG);
+        mCMOVisibilityChangeEnabled = args.getBoolean(MainActivity.CMO_VISIBILITY_CHANGE_TAG);
+    }
+
+    private void setUpMapFragment() {
+        mMapFragment = (CaffeinatedMapFragment) getFragmentManager().findFragmentByTag(MAP_FRAGMENT_TAG);
+        if (mMapFragment == null) {
+            mMapFragment = CaffeinatedMapFragment.newInstance();
+            FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+            fragmentTransaction.add(R.id.container, mMapFragment, MAP_FRAGMENT_TAG).commit();
+        }
+    }
+
+    private void setUpMapIfNeeded() {
+        if (mCaffeinatedMap == null) {
+            mCaffeinatedMap = mMapFragment.getCaffeinatedMap();
+            if (mCaffeinatedMap != null) {
+                mCaffeinatedMap.setMyLocationEnabled(true);
+            }
+        }
+    }
+
+    private void loadSettings() {
+        RandomMarkers randomMarkers = new RandomMarkers(mNumberOfCMO);
+        mCaffeinatedMap.getClusteringSettings().setClusterSize(mClusterSize);
+
+        if (mCaffeinatedClusterIconEnabled) {
+            mCaffeinatedMap.getClusteringSettings().setTypeCountingEnabled(mTypeCountingEnabled);
+
+            final HashMap<String, Integer> colours = new HashMap<String, Integer>();
+            colours.put("Fred", getResources().getColor(R.color.red));
+            colours.put("Jim", getResources().getColor(R.color.green));
+            colours.put("Sally", getResources().getColor(R.color.blue));
+            colours.put("Haley", getResources().getColor(R.color.pink));
+            colours.put("Unknown", getResources().getColor(R.color.black));
+
+            mCaffeinatedMap.getClusteringSettings().setOnClusterIconSelect(new ClusteringSettings.OnClusterIconSelect() {
+                @Override
+                public BitmapDescriptor selectIconFor(Cluster cluster) {
+                    String[] typeList = cluster.getTypeList();
+                    int[] typeCount = new int[typeList.length];
+                    int[] typeColours = new int[typeCount.length];
+                    for (int i = 0; i < typeList.length; i++) {
+                        typeCount[i] = cluster.getTypeCount(typeList[i]);
+                        typeColours[i] = colours.get(typeList[i]);
+                    }
+                    CaffeinatedClusterIcon caffeinatedClusterIcon = new CaffeinatedClusterIcon(getApplicationContext(), null, typeCount, typeColours);
+                    return BitmapDescriptorFactory.fromBitmap(caffeinatedClusterIcon.toBitmap());
+                }
+            });
+        }
+
+        if (mCMOVisibilityChangeEnabled) {
+            mCaffeinatedMap.getClusteringSettings().setOnCMOVisibilityChange(new ClusteringSettings.OnCMOVisibilityChange() {
+                @Override
+                public void onCMOVisible(String key) {
+                    Toast.makeText(getApplicationContext(), "Visible: " + key, Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onCMOHidden(String key) {
+                    Toast.makeText(getApplicationContext(), "Hidden: " + key, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        mCaffeinatedMap.addClusterableMarkers(randomMarkers.getCMOList());
+    }
+
+}
